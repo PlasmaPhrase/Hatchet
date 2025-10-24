b212b54d756e0237dc2fd4261ad33d479ceec874
PlasmaPhrase <plasmaphrase@gmail.com>
2025-10-24 19:28:12 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -24,11 +24,27 @@ SMODS.Atlas({
     atlas_table = "ASSET_ATLAS"
 })
 
+SMODS.Atlas({
+    key = "CustomEnhancements", 
+    path = "CustomEnhancements.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "CustomDecks", 
+    path = "CustomDecks.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
 local NFS = require("nativefs")
 to_big = to_big or function(a) return a end
 lenient_bignum = lenient_bignum or function(a) return a end
 
-local jokerIndexList = {23,4,12,26,9,13,16,27,8,2,17,20,14,18,29,15,3,6,11,5,24,7,22,25,21,10,28,1,30,19}
+local jokerIndexList = {25,13,4,10,14,29,32,30,9,2,17,22,15,18,34,16,3,7,12,6,26,8,24,27,23,11,31,1,35,19,5,20,33,21,28}
 
 local function load_jokers_folder()
     local mod_path = SMODS.current_mod.path
@@ -42,7 +58,39 @@ local function load_jokers_folder()
     end
 end
 
+
+local enhancementIndexList = {3,1,2}
+
+local function load_enhancements_folder()
+    local mod_path = SMODS.current_mod.path
+    local enhancements_path = mod_path .. "/enhancements"
+    local files = NFS.getDirectoryItemsInfo(enhancements_path)
+    for i = 1, #enhancementIndexList do
+        local file_name = files[enhancementIndexList[i]].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("enhancements/" .. file_name))()
+        end
+    end
+end
+
+
+local deckIndexList = {1}
+
+local function load_decks_folder()
+    local mod_path = SMODS.current_mod.path
+    local decks_path = mod_path .. "/decks"
+    local files = NFS.getDirectoryItemsInfo(decks_path)
+    for i = 1, #deckIndexList do
+        local file_name = files[deckIndexList[i]].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("decks/" .. file_name))()
+        end
+    end
+end
+
 load_jokers_folder()
+load_enhancements_folder()
+load_decks_folder()
 SMODS.ObjectType({
     key = "hatchet_food",
     cards = {
@@ -63,14 +111,19 @@ SMODS.ObjectType({
     cards = {
         ["j_hatchet_blackjoker"] = true,
         ["j_hatchet_blueshoes"] = true,
+        ["j_hatchet_composter"] = true,
         ["j_hatchet_d20"] = true,
         ["j_hatchet_dawn"] = true,
         ["j_hatchet_handout"] = true,
         ["j_hatchet_musketeer"] = true,
         ["j_hatchet_needlenonsense"] = true,
+        ["j_hatchet_nightvision"] = true,
+        ["j_hatchet_ninelives"] = true,
+        ["j_hatchet_nodice"] = true,
         ["j_hatchet_pizza"] = true,
         ["j_hatchet_riskyrevolver"] = true,
         ["j_hatchet_stockexchange"] = true,
+        ["j_hatchet_swindler"] = true,
         ["j_hatchet_trickyjoker"] = true
     },
 })
