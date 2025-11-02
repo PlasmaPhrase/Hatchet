21a7442207cefef0b47b6ddaab393e9f49f0afef
PlasmaPhrase <plasmaphrase@gmail.com>
2025-11-02 23:04:14 +0000

    Updated for v1.2
    Orange Deck has been revamped

--- decks/orange_deck.lua
+++ decks/orange_deck.lua
@@ -2,24 +2,22 @@ SMODS.Back {
     key = 'orange_deck',
     pos = { x = 0, y = 0 },
     config = {
-      hand_size = -3,
-        discards = 3,
+      hand_size_value = 6,
+        discards_value = 2,
 },
     loc_txt = {
         name = 'Orange Deck',
         text = {
-            [1] = '{C:attention}-3{} hand size',
-            [2] = '{C:attention}+1{} hand size per Ante',
-            [3] = '{C:red}+3{} discards'
+            [1] = '{C:attention}-2{} hand size',
+            [2] = '{C:red}+2{} discards every round'
         },
     },
     unlocked = true,
     discovered = true,
     no_collection = false,
     atlas = 'CustomDecks',
-    calculate = function(self, card, context)
-        if context.end_of_round and context.game_over == false and context.main_eval and context.beat_boss and not context.blueprint then
-             G.hand:change_size(1)
-        end
-  end,
+     apply = function(self, back)
+            G.GAME.starting_params.hand_size = 6
+            G.GAME.starting_params.discards = G.GAME.starting_params.discards + 2
+    end
 }
