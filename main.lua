f7d4d7c5e54145eb672626528ebf2874071f4951
PlasmaPhrase <plasmaphrase@gmail.com>
2025-11-02 23:05:28 +0000

    Updated for v1.2

--- main.lua
+++ main.lua
@@ -25,6 +25,22 @@ SMODS.Atlas({
 })
 
 SMODS.Atlas({
+    key = "CustomConsumables", 
+    path = "CustomConsumables.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "CustomBoosters", 
+    path = "CustomBoosters.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
     key = "CustomEnhancements", 
     path = "CustomEnhancements.png", 
     px = 71,
@@ -44,7 +60,7 @@ local NFS = require("nativefs")
 to_big = to_big or function(a) return a end
 lenient_bignum = lenient_bignum or function(a) return a end
 
-local jokerIndexList = {25,13,4,10,14,29,32,30,9,2,17,22,15,18,34,16,3,7,12,6,26,8,24,27,23,11,31,1,35,19,5,20,33,21,28}
+local jokerIndexList = {29,16,4,10,17,33,38,34,9,2,21,26,18,22,40,19,3,7,13,6,30,8,28,31,27,12,37,1,41,23,5,24,39,25,32,35,15,20,36,14,11}
 
 local function load_jokers_folder()
     local mod_path = SMODS.current_mod.path
@@ -54,6 +70,32 @@ local function load_jokers_folder()
         local file_name = files[jokerIndexList[i]].name
         if file_name:sub(-4) == ".lua" then
             assert(SMODS.load_file("jokers/" .. file_name))()
+        end
+    end
+end
+
+
+local consumableIndexList = {7,3,1,2,5,10,9,6,11,8,4}
+
+local function load_consumables_folder()
+    local mod_path = SMODS.current_mod.path
+    local consumables_path = mod_path .. "/consumables"
+    local files = NFS.getDirectoryItemsInfo(consumables_path)
+    local set_file_number = #files + 1
+    for i = 1, #files do
+        if files[i].name == "sets.lua" then
+            assert(SMODS.load_file("consumables/sets.lua"))()
+            set_file_number = i
+        end
+    end    
+    for i = 1, #consumableIndexList do
+        local j = consumableIndexList[i]
+        if j >= set_file_number then 
+            j = j + 1
+        end
+        local file_name = files[j].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("consumables/" .. file_name))()
         end
     end
 end
@@ -74,7 +116,7 @@ local function load_enhancements_folder()
 end
 
 
-local deckIndexList = {1}
+local deckIndexList = {2,1}
 
 local function load_decks_folder()
     local mod_path = SMODS.current_mod.path
@@ -88,7 +130,15 @@ local function load_decks_folder()
     end
 end
 
+
+local function load_boosters_file()
+    local mod_path = SMODS.current_mod.path
+    assert(SMODS.load_file("boosters.lua"))()
+end
+
+load_boosters_file()
 load_jokers_folder()
+load_consumables_folder()
 load_enhancements_folder()
 load_decks_folder()
 SMODS.ObjectType({
@@ -114,7 +164,10 @@ SMODS.ObjectType({
         ["j_hatchet_composter"] = true,
         ["j_hatchet_d20"] = true,
         ["j_hatchet_dawn"] = true,
+        ["j_hatchet_etzchaim"] = true,
+        ["j_hatchet_graveyardshift"] = true,
         ["j_hatchet_handout"] = true,
+        ["j_hatchet_loveletter"] = true,
         ["j_hatchet_musketeer"] = true,
         ["j_hatchet_needlenonsense"] = true,
         ["j_hatchet_nightvision"] = true,
@@ -122,6 +175,7 @@ SMODS.ObjectType({
         ["j_hatchet_nodice"] = true,
         ["j_hatchet_pizza"] = true,
         ["j_hatchet_riskyrevolver"] = true,
+        ["j_hatchet_staircase"] = true,
         ["j_hatchet_stockexchange"] = true,
         ["j_hatchet_swindler"] = true,
         ["j_hatchet_trickyjoker"] = true
